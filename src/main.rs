e49bb5df9b0b99cf20c82b6076d6f33c7ff7c641
Elve Johansson <elve2002@live.se>
2023-11-06 21:51:28 +0100

    core: tui foundation
    As tui-rs is no longer maintained, Ratatui is the recommended fork
    to use. Also, idk if Crossterm is the way to go, but it seems like
    it gets the job done.
    
    * https://github.com/fdehau/tui-rs
    * https://github.com/ratatui-org/ratatui
    * https://github.com/crossterm-rs/crossterm
    
    Signed-off-by: Elve Johansson <elve2002@live.se>

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::{error::Error, io::stdout, time::Duration};
+
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{prelude::CrosstermBackend, widgets::Paragraph, Terminal};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    loop {
+        terminal.draw(|frame| {
+            frame.render_widget(Paragraph::new("Hello kubeui!"), frame.size());
+        })?;
+
+        if event::poll(Duration::from_millis(16))? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    return Ok(());
+}
